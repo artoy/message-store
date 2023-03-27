8c7ac166e00c09c3d495d9da2eaa0ca220dea9ae
Ryota Kobayashi <ryota5884@icloud.com>
2023-03-27 23:52:33 +0900

    initial commit

---
+++ message-store/main.py
@@ -0,0 +1,11 @@
+import os
+from slack_bolt import App
+from slack_bolt.adapter.socket_mode import SocketModeHandler
+
+app = App(
+    token=os.environ.get("SLACK_BOT_TOKEN"),
+    signing_secret=os.environ.get("SLACK_SIGNING_SECRET")
+)
+
+if __name__ == "__main__":
+    app.start(port=int(os.environ.get("PORT", 3000)))
